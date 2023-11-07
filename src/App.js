2f44658b12ab272abc293d1c58402cd0e5ff8fd7
kipo3195 <bse3808@gmail.com>
2023-11-07 23:54:27 +0900

    post 방식을 통해 로그인 후 jwt 토큰 전달 받기 완료

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import cookie from 'react-cookies';
 
 function Test(){
   const [item, setItem] = useState([]);
@@ -48,13 +49,56 @@ function TestAsync(){
     </div>
   )
 }
+
+// 로그인 요청 컴포넌트 
+function LoginComponent(props){
+  return(
+      <div>
+        <h2>LoginComponent</h2>
+        <form onSubmit={event=>{
+          event.preventDefault();
+          const userId = event.target.username.value;
+          const password = event.target.password.value;
+          props.loginRequest(userId, password);
+        }}>
+          사용자 id : <input type='text' name='username'  placeholder='사용자 id email 형식'></input>
+          <br/>
+          비밀번호 : <input type='password' name='password' placeholder='비밀번호'></input>
+          <p><input type='submit' value='로그인'></input></p>
+        </form>
+      </div>
+  )
+}
+
 // 주석 추가 
 function App() {
   return (
       <div>
-        <Test></Test>
-        <TestAsync></TestAsync>
-      
+        {/* <Test></Test>
+        <TestAsync></TestAsync> */}
+        <LoginComponent loginRequest={(userId, password)=>{
+          console.log(userId, password)
+           axios({
+            method:'post',
+            url : 'http://localhost:8080/login',
+            data:{
+              "username":userId,
+              "password":password
+            }
+            }).then(function(response){
+              const flag = response.data.flag;
+              if(flag === 'success'){
+                alert("로그인 성공 !")
+                const jwt = response.data.token;
+                console.log(jwt);
+              }else{
+                alert("로그인 실패 !");
+              }
+            }).catch(function(error){
+              console.log(error);
+              alert("로그인 실패 ! 입력한 정보를 확인하세요.");
+            })
+        }}></LoginComponent>
       </div>
   );
 }
