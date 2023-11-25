60f035fedfdfcce0f780f2f58861d81be997287c
kipo3195 <bse3808@gmail.com>
2023-11-25 11:03:27 +0900

    token 만료 로직 이슈 해결 완료 & Promise 객체 사용

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import AccessToken from '../modules/AccessToken';
+
 axios.defaults.withCredentials = true;
 const Home = (props) => {
+    
     return (
         <div className='home'>
-            <h2>안녕하세요 Early 오신것을 환영합니다!</h2>
+            
             <input type="button" value="async test" onClick={()=>{
             axios({
                 method:'GET',
@@ -11,37 +14,55 @@ const Home = (props) => {
                 }).then(function(response){
               
                 const flag = response.data.flag;
-                const error_code = response.data.error_code;
+                const errorCode = response.data.error_code;
 
-                // access token 재발급 로직 
-                if(flag ==='fail' && error_code === '400'){
-                    
-                    axios({
-                        method:'POST',
-                        url:'http://localhost:8080/user/accessToken'
-                    }).then(function(response){
-
-                        const flag = response.data.flag;
-                        if(flag === 'success'){
-                            const accesstoken  = response.data.token;
-                            axios.defaults.headers.common['Authorization'] = `Bearer ${accesstoken}`;
-                        }else if(flag === 'fail'){
-                            const resultCode = response.data.result_code;
-                            alert('로그인 시간이 만료되어 로그인 페이지로 이동합니다.');
-                            axios.defaults.headers.common['Authorization'] = null;
+                if(errorCode == '403'){
+                    // 바로 로그아웃
+                    props.logout();
+                }else{
+                    // refresh 토큰 검증용 로직 
+                    const promise = AccessToken(flag, errorCode);
+                    promise.then(result =>{
+                        if(result ==='logout'){
                             props.logout();
                         }
-                    }).catch(function(response){
-                        console.log(response);
-                    })
-                }else{
-                    // 정상
+                    }).catch(function(error){
+                        console.log(error);
+                    }) 
+
+                    
                 }
+
+                // access token 재발급 로직 
+                // if(flag ==='fail' && error_code === '400'){
+                    
+                //     axios({
+                //         method:'POST',
+                //         url:'http://localhost:8080/user/accessToken'
+                //     }).then(function(response){
+
+                //         const flag = response.data.flag;
+                //         if(flag === 'success'){
+                //             const accesstoken  = response.data.token;
+                //             axios.defaults.headers.common['Authorization'] = `Bearer ${accesstoken}`;
+                //         }else if(flag === 'fail'){
+                //             const resultCode = response.data.result_code;
+                //             alert('로그인 시간이 만료되어 로그인 페이지로 이동합니다.');
+                //             axios.defaults.headers.common['Authorization'] = null;
+                //             props.logout();
+                //         }
+                //     }).catch(function(response){
+                //         console.log(response);
+                //     })
+                // }else{
+                //     // 정상
+                // }
                 }).catch(function(error){
                 console.log(error);
                 })
         
-              }}/>
+              }}
+              />
 
         </div>
     )
---
+++ src/modules/AccessToken.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+
+
+
+
+async function AccessToken (_flag, _errorCode){
+    
+    var result = null;
+    // console.log('AccessToken', _flag, _errorCode);
+    // access token 재발급 로직 
+    // axios는 기본적으로 비동기 처리 라이브러리.
+    // 비동기 처리이기 때문에 accessToken 수행 후 result에 값을 할당 하기도 전에 return해 버리는 것이 문제.
+    // 그래서 동기 처리를 가능하게 하는 await 를 사용하기 위해 async를 사용(await는 async함수 안에 존재 해야하는 것이 규칙)
+    // 단 await를 사용하면 자동으로 Promise 객체를 만드는 것 처럼 보임. 
+    // 그래서 AccessToken을 호출 하는 곳에서 return 데이터를 확인해 보면 Promise 객체가 반환 되는 것을 확인함. 
+    // Promise 객체의 PromiseResult에 접근해서 데이터를 사용하면 된다. .then으로 접근 할 수 있다.
+
+        if(_flag ==='fail' && _errorCode === '400'){
+            await axios({
+                method:'POST',
+                url:'http://localhost:8080/user/accessToken'
+            }).then(function(response){
+                const flag = response.data.flag;
+                if(flag === 'success'){
+                    // 성공
+                    const accesstoken  = response.data.token;
+                    axios.defaults.headers.common['Authorization'] = `Bearer ${accesstoken}`;
+                }else if(flag === 'fail'){
+                    result = 'logout';
+                    axios.defaults.headers.common['Authorization'] = null;
+                    alert('로그인 기간이 만료되어 로그아웃 됩니다.')
+                }
+            }).catch(function(error){
+                console.log(error);
+            })
+
+        }else{
+            // 만료안됨
+        }
+
+        return result;
+
+}
+
+export default AccessToken;
